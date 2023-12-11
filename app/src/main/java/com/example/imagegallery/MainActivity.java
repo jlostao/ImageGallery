a046d7afb010562891a4ab3290eaf7b462efe51a
jordi <jorlosnav@gmail.com>
2023-12-11 16:23:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/imagegallery/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.imagegallery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize ActivityResultLauncher in onCreate
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageURI(uri);
+                        }
+                    }
+                });
+    }
+
+    public void openSomeActivityForResult(View view) {
+        // Create Intent
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        // Launch activity to get result using the initialized ActivityResultLauncher
+        someActivityResultLauncher.launch(intent);
+    }
+}
